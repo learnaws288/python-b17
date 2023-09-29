2fbe470669fe96320582056f0adbbe28cfe04075
learnaws288 <142102664+learnaws288@users.noreply.github.com>
2023-09-29 12:17:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask Application - Ashok IT"
+    return "Welcome to Python Flask Application"
 
 
 if __name__ == "__main__":
